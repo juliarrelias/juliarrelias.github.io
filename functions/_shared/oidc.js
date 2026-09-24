9cb9d35163eb48ef141cabec9db42c0b7cd16022
juliarrelias <anajulia.arrelias@gmail.com>
2026-09-24 19:59:42 -0300

    Add OIDC functions for code exchange and identity confirmation

---
+++ functions/_shared/oidc.js
@@ -0,0 +1,7 @@
+export async function exchangeCodeForToken(context, provider, { code, codeVerifier, redirectUri }) {
+  throw new Error("exchangeCodeForToken ainda não implementado (seção 13.5)");
+}
+
+export async function confirmIdentity(context, provider, tokenResponse, { nonce }) {
+  throw new Error("confirmIdentity ainda não implementado (seção 13.5)");
+}
