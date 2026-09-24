3b857f397e36c6f6f13d5e0c6c6c76b803ce0ea7
juliarrelias <anajulia.arrelias@gmail.com>
2026-09-24 20:04:59 -0300

    Add client ID and secret environment variables
    Added clientIdEnv and clientSecretEnv for Google and GitHub.

--- functions/_shared/providers.js
+++ functions/_shared/providers.js
@@ -1,13 +1,19 @@
 export const PROVIDERS = {
   google: {
     authorizationEndpoint: "https://accounts.google.com/o/oauth2/v2/auth",
+    tokenEndpoint: "https://oauth2.googleapis.com/token",
     scope: "openid email profile",
-    usesNonce: true
+    usesNonce: true,
+    clientIdEnv: "GOOGLE_CLIENT_ID",
+    clientSecretEnv: "GOOGLE_CLIENT_SECRET"
   },
   github: {
     authorizationEndpoint: "https://github.com/login/oauth/authorize",
+    tokenEndpoint: "https://github.com/login/oauth/access_token",
     scope: null,
-    usesNonce: false
+    usesNonce: false,
+    clientIdEnv: "GITHUB_CLIENT_ID",
+    clientSecretEnv: "GITHUB_CLIENT_SECRET"
   }
 };
 
